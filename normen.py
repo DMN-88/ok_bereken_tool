2e59cf2dfbb37d2fe5f8b40c771394d219195406
DMN-88 <r.r.aitatus@gmail.com>
2025-03-21 15:39:25 +0100

    Update normen.py

--- normen.py
+++ normen.py
@@ -1,16 +1,31 @@
-vccn_normen = {
-    "Operatiekamer": 20,
-    "Opdekruimte": 15,
-    "Sluis": 10,
-    "Wasruimte": 8,
-    "Opslag": 5,
-    "OK-Gang": None,
-    "CSA-Schoon": 8,
-    "CSA-Vuil": 6,
-    "Steriele Opslag": 4,
-    "Niet Steriele Opslag": None,
-    "Recovery": 6,
-    "Holding": 6,
-    "OK-Kantoor": None,
-    "Ruimte Algemeen": None
+ruimte_classificatie_normen = {
+    "Operatiekamer": {
+        "Hoogrisico": {"luchtwisselingen": 20, "iso_klasse": "ISO 7"},
+        "Laagrisico": {"luchtwisselingen": 6, "iso_klasse": "n.v.t."}
+    },
+    "Opdekruimte": {
+        "Standaard": {"luchtwisselingen": 20, "iso_klasse": "ISO 7"}
+    },
+    "Behandelkamer": {
+        "Laagrisico": {"luchtwisselingen": 6, "iso_klasse": "n.v.t."}
+    },
+    "Recovery": {
+        "Standaard": {"luchtwisselingen": 6, "iso_klasse": "-"}
+    },
+    "CSA-Schoon": {
+        "Standaard": {"luchtwisselingen": 8, "iso_klasse": "-"}
+    },
+    "CSA-Vuil": {
+        "Standaard": {"luchtwisselingen": 6, "iso_klasse": "-"}
+    },
+    "Wasruimte": {
+        "Standaard": {"luchtwisselingen": 8, "iso_klasse": "-"}
+    },
+    "Steriele Opslag": {
+        "Standaard": {"luchtwisselingen": 4, "iso_klasse": "-"}
+    },
+    "Niet Steriele Opslag": {},
+    "OK-Gang": {},
+    "OK-Kantoor": {},
+    "Ruimte Algemeen": {}
 }
