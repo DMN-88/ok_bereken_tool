9492c57b46e192d8b5bf6bd74ac7400d0e0148b4
DMN-88 <r.r.aitatus@gmail.com>
2025-03-21 15:25:20 +0100

    Update berekeningen.py

--- berekeningen.py
+++ berekeningen.py
@@ -4,10 +4,17 @@ def bereken_luchtdebiet(opp, hoogte, luchtwisselingen):
     return volume, luchtdebiet
 
 def bereken_warmtevermogen(pers, apparatuur, verlichting):
-    warmte_persoon = pers * 120  # W per persoon
+    warmte_persoon = pers * 120  # W
     totaal_watt = warmte_persoon + apparatuur + verlichting
     return totaal_watt / 1000  # kW
 
 def bereken_bevochtiging(luchtdebiet, rv_gewenst, rv_buiten, temp):
     bevochtiging = (rv_gewenst - rv_buiten) * luchtdebiet * 0.001
     return max(bevochtiging, 0)
+
+def bereken_conditioneringsvermogen(luchtdebiet_m3h, delta_T):
+    rho = 1.2  # kg/m³
+    c = 1005   # J/kg·K
+    V_m3s = luchtdebiet_m3h / 3600
+    Q_watt = rho * V_m3s * c * delta_T
+    return round(Q_watt / 1000, 2)  # kW
