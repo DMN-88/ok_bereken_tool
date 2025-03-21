f23345fc1b0c5b5dfcfed7ae00d8840c284fd34c
DMN-88 <r.r.aitatus@gmail.com>
2025-03-21 15:21:03 +0100

    Update normen.py

--- normen.py
+++ normen.py
@@ -3,5 +3,14 @@ vccn_normen = {
     "Opdekruimte": 15,
     "Sluis": 10,
     "Wasruimte": 8,
-    "Opslag": 5
+    "Opslag": 5,
+    "OK-Gang": None,
+    "CSA-Schoon": 8,
+    "CSA-Vuil": 6,
+    "Steriele Opslag": 4,
+    "Niet Steriele Opslag": None,
+    "Recovery": 6,
+    "Holding": 6,
+    "OK-Kantoor": None,
+    "Ruimte Algemeen": None
 }
